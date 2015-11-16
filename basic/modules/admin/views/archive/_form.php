268e0c2a0190c5d3b8babaadf7435dea6d9db700
Aleksandr Tikhonov <post@larek.pro>
2015-11-17 00:27:26 +0300

    image manager in tinymce

--- basic/modules/admin/views/archive/_form.php
+++ basic/modules/admin/views/archive/_form.php
@@ -34,7 +34,7 @@ $(function() {
         'plugins' => [
             "advlist autolink lists link charmap print preview anchor",
             "searchreplace visualblocks code fullscreen",
-            "insertdatetime media table contextmenu paste"
+            "insertdatetime media table contextmenu paste image"
         ],
         'toolbar' => "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify  | bullist numlist outdent indent | link image"
     ]
--- basic/modules/admin/views/rent/_form.php
+++ basic/modules/admin/views/rent/_form.php
@@ -24,7 +24,7 @@ use dosamigos\tinymce\TinyMce;
         'plugins' => [
             "advlist autolink lists link charmap print preview anchor",
             "searchreplace visualblocks code fullscreen",
-            "insertdatetime media table contextmenu paste"
+            "insertdatetime media table contextmenu paste image"
         ],
         'toolbar' => "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify  | bullist numlist outdent indent | link image"
     ]
@@ -37,7 +37,7 @@ use dosamigos\tinymce\TinyMce;
         'plugins' => [
             "advlist autolink lists link charmap print preview anchor",
             "searchreplace visualblocks code fullscreen",
-            "insertdatetime media table contextmenu paste"
+            "insertdatetime media table contextmenu paste image"
         ],
         'toolbar' => "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify  | bullist numlist outdent indent | link image"
     ]
@@ -50,7 +50,7 @@ use dosamigos\tinymce\TinyMce;
         'plugins' => [
             "advlist autolink lists link charmap print preview anchor",
             "searchreplace visualblocks code fullscreen",
-            "insertdatetime media table contextmenu paste"
+            "insertdatetime media table contextmenu paste image"
         ],
         'toolbar' => "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify  | bullist numlist outdent indent | link image"
     ]
@@ -63,7 +63,7 @@ use dosamigos\tinymce\TinyMce;
         'plugins' => [
             "advlist autolink lists link charmap print preview anchor",
             "searchreplace visualblocks code fullscreen",
-            "insertdatetime media table contextmenu paste"
+            "insertdatetime media table contextmenu paste image"
         ],
         'toolbar' => "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify  | bullist numlist outdent indent | link image"
     ]
