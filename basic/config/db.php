169d10883a827126f4c9cb287e954d776e5d246b
Alexandr Tikhonov <t9101029991@gmail.com>
2016-02-11 16:04:06 +0300

    gitignore

--- basic/config/db.php
+++ basic/config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=rd-nnov',
-    'username' => 'rootq',
+    'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
 ];
