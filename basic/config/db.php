c64329d2e1d7ae3c40a0890e78a34d55c3d6945d
Alexandr Tikhonov <t9101029991@gmail.com>
2016-02-11 16:08:02 +0300

    ignore

--- basic/config/db.php
+++ basic/config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=rd-nnov',
-    'username' => 'root',
+    'username' => 'rootsdf',
     'password' => 'root',
     'charset' => 'utf8',
 ];
