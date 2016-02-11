f6031fb30be21cf4efff85bb3553264498f23db5
Alexandr Tikhonov <t9101029991@gmail.com>
2016-02-11 16:00:32 +0300

    add gitignore in root

--- basic/config/db.php
+++ basic/config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=rd-nnov',
-    'username' => 'root',
+    'username' => 'rootq',
     'password' => 'root',
     'charset' => 'utf8',
 ];
