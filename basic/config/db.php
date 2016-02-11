bf5914c6c9766ce95aeb770171f207b829a18038
Alexandr Tikhonov <t9101029991@gmail.com>
2016-02-11 16:13:34 +0300

    git ignore db config

--- basic/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=rd-nnov',
-    'username' => 'rootsdf',
-    'password' => 'root',
-    'charset' => 'utf8',
-];
