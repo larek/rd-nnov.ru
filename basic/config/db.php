904ef22e8f3aa4d9953476deb68478bdf3ac242c
Alexandr Tikhonov <t9101029991@gmail.com>
2016-02-11 17:53:28 +0300

    db config

---
+++ basic/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=rd-nnov',
+    'username' => 'rd-nnov',
+    'password' => 'avatar910',
+    'charset' => 'utf8',
+];
