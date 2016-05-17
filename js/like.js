1222704852e3d336ea49984269d9503f5dccd11d
Alexandr Tikhonov <t9101029991@gmail.com>
2016-05-18 02:30:00 +0300

    correct get path in like.js

--- js/like.js
+++ js/like.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     $(".btn-update-like").click(function() {
         boy = $(".like-boy").val();
         girl = $(".like-girl").val();
-        $.get("like/update", { 'boy': boy, 'girl': girl }).done(function(data) {
+        $.get("/admin/like/update", { 'boy': boy, 'girl': girl }).done(function(data) {
             window.location = window.location;
         });
     });
