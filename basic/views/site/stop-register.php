91207e7663b6a99b2934320667e0a073a853ea78
Aleksandr Tikhonov <post@larek.pro>
2016-05-20 13:06:59 +0300

    stop-register

--- basic/views/site/stop-register.php
+++ basic/views/site/stop-register.php
@@ -1,6 +1,6 @@
-<div class='alert alert-danger hide'>
+<div class='alert alert-danger '>
 <span class='glyphicon glyphicon glyphicon-exclamation-sign'></span> Прием анкет от ресторанов на размещение в бумажной карте остановлен. Мы приступаем к созданию карты. Всем спасибо за участие.
 </div>
-<div class='alert alert-danger'>
+<div class='alert alert-danger hide'>
 <span class='glyphicon glyphicon glyphicon-exclamation-sign'></span> Регистрация ресторанов на майский ресторанный день пока закрыта. Следите за новостями в <a href="https://vk.com/restaurantday_nn" target='_blank'>группе ВК</a>
 </div>
