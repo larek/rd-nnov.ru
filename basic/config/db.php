c990cdacdfd28da4b2fb7bda9f9f4f3d8b1889ab
Alexandr Tikhonov <t9101029991@gmail.com>
2017-04-02 22:25:09 +0300

    add gitignore rule

--- basic/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=rd-nnov',
-    'username' => 'rd-nnov',
-    'password' => 'avatar910',
-    'charset' => 'utf8',
-];
