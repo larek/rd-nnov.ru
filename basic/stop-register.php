898308c2ff1261c78929b1e0d7bedc3c5ee44f59
Alexandr Tikhonov <t9101029991@gmail.com>
2017-05-18 14:32:20 +0300

    rm stop-register.php

--- basic/stop-register.php
+++
@@ -1,3 +0,0 @@
-<div class='alert alert-danger'>
-<span class='glyphicon glyphicon glyphicon-exclamation-sign'></span> Прием анкет от ресторанов на размещение в бумажной карте остановлено. Мы приступаем к созданию карты. Всем спасибо за участие!
-</div>
