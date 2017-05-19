bd12c8003e5d7f4e4aed6590a59f8d793b37f286
Alexandr Tikhonov <t9101029991@gmail.com>
2017-05-20 02:24:44 +0300

    resoran mobile map compress concat css js

---
+++ map/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no'>
+	<title>Document</title>
+	<link rel="stylesheet" href="boundle.css">
+</head>
+<body>
+	<div id="map"></div>
+	<div id="content"></div>
+	<script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
+	<script src="boundle.js"></script>
+</body>
+</html>
