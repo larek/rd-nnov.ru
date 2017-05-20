ae63ef6b14bb34d67e772435aed6aeddf82a1279
Alexandr Tikhonov <t9101029991@gmail.com>
2017-05-20 04:22:14 +0300

    correct notification

--- map/index.php
+++ map/index.php
@@ -13,7 +13,7 @@
 	<div id="content"></div>
 	</div>
 	<div class="overlay">
-		Веб-приложение работает в только портретной ариентации на мобильных телефонах
+		Веб-приложение работает в портретной ариентации на мобильных телефонах
 	</div>
 	<script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
 	<script src="boundle.js"></script>
