434405ca669e291c0e9faa8f85c960ae2b5c8600
Alexandr Tikhonov <t9101029991@gmail.com>
2017-08-01 11:40:17 +0300

    new front banner

--- basic/assets/AppAsset.php
+++ basic/assets/AppAsset.php
@@ -18,7 +18,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'css/site.css?v=1.0.2',
         'css/OpenSans.css',
     ];
     public $js = [
