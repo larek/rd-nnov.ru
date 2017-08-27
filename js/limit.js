12d9d5638384fcd2c2bd148e2c2c169f8e639489
antonk52 <antonk52@icloud.com>
2017-08-27 04:11:03 +0300

    cache $ objects

--- js/limit.js
+++ js/limit.js
@@ -1,16 +1,14 @@
 function limitChars(textid, limit, infodiv){
-    var text = $('#'+textid).val(); 
-    var textlength = text.length;
-    if(textlength > limit)
-    {
-    $('#' + infodiv).html('Вам нельзя написать более чем '+limit+' символов!');
-    $('#'+textid).val(text.substr(0,limit));
-    return false;
-    }
-    else
-    {
-    $('#' + infodiv).html('У Вас осталось '+ (limit - textlength) +' символов.');
-    return true;
+    var $textId = $('#' + textid);
+    var $infoDiv = $('#' + infodiv);
+    var textlength = $textId.val().length;
+    if (textlength > limit) {
+        $infoDiv.html('Вам нельзя написать более чем ' + limit + ' символов!');
+        $textId.val(text.substr(0,limit));
+        return false;
+    } else {
+        $infoDiv.html('У Вас осталось '+ (limit - textlength) + ' символов.');
+        return true;
     }
 }
 
@@ -18,9 +16,9 @@ $(function(){
     $('#concept').keyup(function(){
         limitChars('concept', 140, 'infodiv-concept');
     });
-    
+
     $('#menu').keyup(function(){
         limitChars('menu', 140, 'infodiv-menu');
-    })
-    
+    });
+
 });
